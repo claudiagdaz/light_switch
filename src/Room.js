b75548dbb71a842242443679d905c2219d2101ed
Claudia Davila <claudiagdaz@gmail.com>
2024-08-07 18:34:50 -0600

    Deployment test after commit

--- src/Room.js
+++ src/Room.js
@@ -1,28 +1,36 @@
 import { useState } from "react";
-import './Room.css';
-import { GoSun, GoMoon, GoLightBulb } from 'react-icons/go'
+import "./Room.css";
+import { GoSun, GoMoon, GoLightBulb } from "react-icons/go";
 
-function Room() {    
-    const [isLit, setIsLit] = useState(false);
-    const [temperature, setTemperature] = useState(22)
-    const brightness = isLit? "lit" : "dark"
-   
-    return (
-        <div className={`room ${brightness}`}>
-            the room is {brightness} and {temperature}°C
-            <br/>
-            <button onClick={() => setIsLit(!isLit)}>
-                Flip the switch <GoLightBulb />
-            </button>
-            <button onClick={() => setIsLit(true)}><GoSun /> </button>
-            <button onClick={() => setIsLit(false)}><GoMoon/></button>
-            <br />
-            <button onClick={() => setTemperature(temperature + 1)}> Turn on the heater</button>
-            <button onClick={() => setTemperature(temperature - 1)}> Turn on the AC</button>
- 
-        </div>
-    )
+function Room() {
+  const [isLit, setIsLit] = useState(false);
+  const [temperature, setTemperature] = useState(22);
+  const brightness = isLit ? "lit" : "dark";
 
+  return (
+    <div className={`room ${brightness}`}>
+      the room is {brightness} and {temperature}°C
+      <br />
+      <button onClick={() => setIsLit(!isLit)}>
+        Flip the switch <GoLightBulb />
+      </button>
+      <button onClick={() => setIsLit(true)}>
+        <GoSun />{" "}
+      </button>
+      <button onClick={() => setIsLit(false)}>
+        <GoMoon />
+      </button>
+      <br />
+      <button onClick={() => setTemperature(temperature + 1)}>
+        {" "}
+        Turn off the AC
+      </button>
+      <button onClick={() => setTemperature(temperature - 1)}>
+        {" "}
+        Turn on the AC
+      </button>
+    </div>
+  );
 }
 
 export default Room;
